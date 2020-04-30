0e07a2a9e4030b3944bfc14e16512f6c79c037fb
jooodai <59430113+jooodai@users.noreply.github.com>
2020-04-30 17:20:49 +0300

    Начало второго урока

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username':'Tolya'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
+
+
 
 
