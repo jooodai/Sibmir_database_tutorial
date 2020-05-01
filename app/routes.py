a7ef7acd461a9f2a0693e7de08e87021efb1f842
jooodai <59430113+jooodai@users.noreply.github.com>
2020-05-01 20:27:06 +0300

    работа с 3-им уроком.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -10,9 +10,13 @@ def index():
     cars = ['Ford', 'Opel', 'Kia']
     return render_template('index.html', user=user, cars=cars)
 
-@app.route('/login')
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
 
