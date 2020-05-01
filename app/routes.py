bec1166442f315e0d993102f28837cc9f895c772
jooodai <59430113+jooodai@users.noreply.github.com>
2020-05-01 18:32:48 +0300

    работа с 3-им уроком.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -9,6 +10,10 @@ def index():
     cars = ['Ford', 'Opel', 'Kia']
     return render_template('index.html', user=user, cars=cars)
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 
 
